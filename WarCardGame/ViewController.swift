f35f9e1ac462256a4a4417196a9a19d8a6abbf0c
Jesse Du <thebestbestman2@hotmail.com>
2020-04-26 12:52:21 -0400

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Jesse Du on 4/26/20.
+//  Copyright © 2020 Jesse Du. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
